e04f76cccbef3e2a77dbef0cf7dc382cd91efab9
Kingandred <95139685+Kingandred@users.noreply.github.com>
2022-03-25 11:38:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,52 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_wdith, screen_height))
 pygame.display.set_caption("Space Invaders")
 
+#define colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
 #load image
 bg = pygame.image.load("")
 
 def draw_bg():
   screen.blit(bg, (0, 0))
+
+
+#create spaceship class
+class Spaceship(pygame.sprite.Sprite):
+  def __init__(self, x, y, health):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load("")
+    self.rect = self.image.get_rect()
+    self.rect.center = [x, y]
+    self.health_start = health
+    self.health_remaining = health
+    
+    
+  def update(self):
+    #set movement speed
+    speed = 8
+    
+    #get key press
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and self.rect.left > 0:
+      self.rect.x -= speed
+    if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+      self.rect.x += speed
+      
+    #draw health bar
+    pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
+    if self.health_remaining > 0:
+      pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health.remaining / self.health_start)), 15))
+      
+   
+#create sprite groups
+spaceship_group = pygame.sprite.Group()
+
+#create player
+spaceship = Spaceship(int(screen_width/2), screen_height - 100, 3)
+spaceship_group.append(spaceship)
 
 run = True
 while run:
@@ -29,6 +70,15 @@ while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
+      
+      
+  #update spaceship
+  spaceship.update()
+      
+      
+  #draw sprite groups
+  spaceship_group.draw(screen)
+  
       
       
   pygame.display.update()
